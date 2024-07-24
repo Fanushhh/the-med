c8710817d7cc348463099f1a2d4aede95217349c
Fanush <fgabriel.mihailescu@gmail.com>
2024-07-24 11:20:19 +0300

    fix: favicon attempt #32

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,11 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="ro">
       <head>
-        <link
-          rel="icon"
-          href="/images/favicon-the-med.ico"
-          sizes="any"
-        />
+       
         <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
 <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
 <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
