c3e876c331098107c10cd2d933d2aaddd8b1e36f
Fanush <fgabriel.mihailescu@gmail.com>
2024-07-24 11:59:37 +0300

    fix: favicon icon attempt #40

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,11 +14,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="ro">
       <head>
-  <link rel="icon" href="https://the-med-new.vercel.app/images/favicon-the-med.ico" sizes="any" />
-  <link rel="apple-touch-icon" sizes="180x180" href="https://the-med-new.vercel.app/images/apple-touch-icon.png" />
-  <link rel="icon" type="image/png" sizes="32x32" href="https://the-med-new.vercel.app/images/favicon-32x32.png" />
-  <link rel="icon" type="image/png" sizes="16x16" href="https://the-med-new.vercel.app/images/favicon-16x16.png" />
-  <link rel="manifest" href="https://the-med-new.vercel.app/images/site.webmanifest" />
+  <link rel="icon" href="/favicon-the-med.ico" sizes="any" />
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+  <link rel="manifest" href="/site.webmanifest" />
 </head>
 
 
